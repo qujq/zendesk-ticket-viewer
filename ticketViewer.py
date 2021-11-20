c044df7ab17602e90aeeca69c616ea5ff5e67e30
qujq-ubuntu <qujq@outlook.com>
2021-11-20 00:37:44 -0800

    feat: get and display ticket list

--- ticketViewer.py
+++ ticketViewer.py
@@ -1,7 +1,91 @@
-from flask import Flask
+import requests
+import json
+from geolib import geohash
+from flask import Flask, request, jsonify
+
+USER = 'junquanq@usc.edu'
+PWD = 'K*$!vNc9$4Br.8p'
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return app.send_static_file('base.html')
+
+@app.route('/getTicket', methods=["GET"])
+def get_ticket():
+    # Set the request parameters
+    url = 'https://zcczendeskcodingchallenge5191.zendesk.com/api/v2/tickets.json?include=comment_count'
+    user = USER
+    pwd = PWD
+
+    # Do the HTTP get request
+    response = requests.get(url, auth=(user, pwd))
+
+    # Check for HTTP codes other than 200
+    if response.status_code != 200:
+        print('Status:', response.status_code, 'Problem with the request. Exiting.')
+        exit()
+
+    # Decode the JSON response into a dictionary and use the data
+    data = response.json()
+    print(data)
+    data = jsonify(data)
+    data.headers['Access-Control-Allow-Origin'] = '*'
+    return data
+
+@app.route('/getUserName', methods=["GET"])
+def get_user_name():
+    # Set the request parameters
+    
+    arg = request.args
+    user_data = arg.to_dict()
+    user_id = user_data.get("user_id")
+    url = 'https://zcczendeskcodingchallenge5191.zendesk.com/api/v2/users/'
+    url += str(user_id)
+
+    # Do the HTTP get request
+    response = requests.get(url, auth=(USER, PWD))
+    
+    # Check for HTTP codes other than 200
+    if response.status_code != 200:
+        print('Status:', response.status_code, 'Problem with the request. Exiting.')
+        exit()
+
+    # Decode the JSON response into a dictionary and use the data
+    data = response.json()
+    print(data)
+    data = jsonify(data)
+    data.headers['Access-Control-Allow-Origin'] = '*'
+    return data
+
+@app.route('/getSelectedTicket', methods=["GET"])
+def get_selected_ticket():
+    # Set the request parameters
+    
+    # user = USER
+    # pwd = PWD
+
+    arg = request.args
+    ticket_data = arg.to_dict()
+    ticket_id = ticket_data.get("ticket_id")
+    url = 'https://zcczendeskcodingchallenge5191.zendesk.com/api/v2/tickets/'
+    url += ticket_id
+
+    # Do the HTTP get request
+    response = requests.get(url, auth=(USER, PWD))
+
+    # Check for HTTP codes other than 200
+    if response.status_code != 200:
+        print('Status:', response.status_code, 'Problem with the request. Exiting.')
+        exit()
+
+    # Decode the JSON response into a dictionary and use the data
+    data = response.json()
+    print(data)
+    data = jsonify(data)
+    data.headers['Access-Control-Allow-Origin'] = '*'
+    return data
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8000, debug=True)
