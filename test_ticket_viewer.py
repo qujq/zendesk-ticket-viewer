1479bb4227c1db484bedc4b506cd29b4177dd6c3
qujq-ubuntu <qujq@outlook.com>
2021-11-24 20:46:48 -0800

    feat: add setUp (get ticket list) for unittest

--- test_ticket_viewer.py
+++ test_ticket_viewer.py
@@ -4,15 +4,20 @@ from ticket_viewer import app
 
 app.testing = True
 
+with open('usernamePassword.json', 'r') as f:
+    usernameAndPassword = json.load(f)
+    username = usernameAndPassword["username"]
+    password = usernameAndPassword["password"]
+
 class TestTicketViewer(unittest.TestCase):
-    def test_get_ticket(self):
-        with app.test_client() as client:
-            with open('usernamePassword.json', 'r') as f:
-                usernameAndPassword = json.load(f)
-                username = usernameAndPassword["username"]
-                password = usernameAndPassword["password"]
-                response = client.get("/getTicket?user=" + username + "&pwd=" + password)
-                response_data = json.loads(response.data)
-                assert response.status_code == 200
-                assert "tickets" in response_data.keys() and "count" in response_data.keys()
-              
+    response = ''
+    response_data = ''
+    def setUp(self):
+        with app.test_client() as client: 
+            self.response = client.get("/getTicket?user=" + username + "&pwd=" + password)
+            self.response_data = json.loads(self.response.data)
+    
+    def test_get_ticket(self):        
+        assert self.response.status_code == 200
+        assert "tickets" in self.response_data.keys() and "count" in self.response_data.keys()
+    
