d1b4909dca4ec4ba7279fedd60bb1d6831c5cd81
qujq-ubuntu <qujq@outlook.com>
2021-11-24 21:57:38 -0800

    fix: remove some redundant parts, add getting username and pwd from request args for testing

--- ticket_viewer.py
+++ ticket_viewer.py
@@ -2,9 +2,6 @@ import requests
 import json
 from flask import Flask, request, jsonify
 
-USER = ''
-PWD = ''
-
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,19 +10,23 @@ def hello():
 
 @app.route('/getTicket', methods=["GET"])
 def get_ticket():
-    # Set the request parameters
     url = 'https://zcczendeskcodingchallenge5191.zendesk.com/api/v2/tickets.json?include=comment_count'
+    
+    # Set default username, password, page index, and page size
     user = USER
     pwd = PWD
     page_index = '1'
     per_page = '25'
+
+    # Set the request parameters
     arg = request.args
     args_data = arg.to_dict()
-    if(args_data.__contains__("page")): page_index = args_data.get("page")
-    if(args_data.__contains__("per_page")): per_page = args_data.get("per_page")
-    if(args_data.__contains__("user")): user = args_data.get("user")
-    if(args_data.__contains__("pwd")): pwd = args_data.get("pwd")
-    print(page_index, per_page, user, pwd)
+    if args_data.__contains__("page"): page_index = args_data.get("page")
+    if args_data.__contains__("per_page"): per_page = args_data.get("per_page")
+    if args_data.__contains__("user"): user = args_data.get("user")
+    if args_data.__contains__("pwd"): pwd = args_data.get("pwd")
+
+    # pagination
     url += "&page=" + page_index + "&per_page=" + per_page
 
     # Do the HTTP get request
@@ -39,23 +40,28 @@ def get_ticket():
 
     # Decode the JSON response into a dictionary and use the data
     data = response.json()
-    print(data)
     data = jsonify(data)
     data.headers['Access-Control-Allow-Origin'] = '*'
     return data
 
 @app.route('/getUserName', methods=["GET"])
 def get_user_name():
+    # Set default username and password
+    user = USER
+    pwd = PWD
+
     # Set the request parameters
-    
     arg = request.args
-    user_data = arg.to_dict()
-    user_id = user_data.get("user_id")
+    args_data = arg.to_dict()
+    user_id = args_data.get("user_id")
+    if args_data.__contains__("user"): user = args_data.get("user")
+    if args_data.__contains__("pwd"): pwd = args_data.get("pwd")
+
     url = 'https://zcczendeskcodingchallenge5191.zendesk.com/api/v2/users/'
     url += str(user_id)
 
     # Do the HTTP get request
-    response = requests.get(url, auth=(USER, PWD))
+    response = requests.get(url, auth=(user, pwd))
     
     # Check for HTTP codes other than 200
     if response.status_code != 200:
@@ -64,22 +70,28 @@ def get_user_name():
 
     # Decode the JSON response into a dictionary and use the data
     data = response.json()
-    print(data)
     data = jsonify(data)
     data.headers['Access-Control-Allow-Origin'] = '*'
     return data
 
 @app.route('/getSelectedTicket', methods=["GET"])
 def get_selected_ticket():
+    # Set default username and password
+    user = USER
+    pwd = PWD
+
     # Set the request parameters
     arg = request.args
-    ticket_data = arg.to_dict()
-    ticket_id = ticket_data.get("ticket_id")
+    args_data = arg.to_dict()
+    ticket_id = args_data.get("ticket_id")
+    if args_data.__contains__("user"): user = args_data.get("user")
+    if args_data.__contains__("pwd"): pwd = args_data.get("pwd")
+
     url = 'https://zcczendeskcodingchallenge5191.zendesk.com/api/v2/tickets/'
     url += ticket_id
-
+    
     # Do the HTTP get request
-    response = requests.get(url, auth=(USER, PWD))
+    response = requests.get(url, auth=(user, pwd))
 
     # Check for HTTP codes other than 200
     if response.status_code != 200:
@@ -88,7 +100,6 @@ def get_selected_ticket():
 
     # Decode the JSON response into a dictionary and use the data
     data = response.json()
-    print(data)
     data = jsonify(data)
     data.headers['Access-Control-Allow-Origin'] = '*'
     return data
